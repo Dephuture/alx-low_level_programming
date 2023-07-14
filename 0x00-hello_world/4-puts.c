6773cab7cdf7d3497795e50b859f7c28f6b9dc82
Dephuture <tenkorangbright@gmail.com>
2023-07-13 19:00:00 -0700

    gg

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - This is the main entry
+*return: This must always return 0 for success
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
