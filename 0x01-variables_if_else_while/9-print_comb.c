a38f30e4574a5d46c3a138b69dee6d9ca907a968
Dephuture <tenkorangbright@gmail.com>
2023-07-15 10:11:09 -0700

    m

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Start point
+ * Description: Print Alpabet anf Numbers
+ * Return: Should always be 0 for success
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
