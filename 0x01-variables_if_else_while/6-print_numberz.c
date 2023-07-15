a70746f77d3fc3b4e34556e39956d4b1d992c613
Dephuture <tenkorangbright@gmail.com>
2023-07-15 08:51:32 -0700

    hh

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Starting point
+ * Description: print 1-0
+ * Return: Always 0 for success
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n >= 9; n++)
+	{
+		putchar(n + 48);
+	}
+	putchar('\n');
+	return (0);
+}
