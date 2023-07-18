b1fccf3598065ce9c59ee51c7c3f48beb278bf26
Dephuture <tenkorangbright@gmail.com>
2023-07-18 16:18:01 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - this task is part of the function projects
+ * Return: Always return 0 for success
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/explaination.c
@@ -0,0 +1,7 @@
+#include "main.h"
+/**
+ * main- This is the starting point
+ * Description: this task is part of the function projects
+ * Return: Always return 0 for success
+ */
+
