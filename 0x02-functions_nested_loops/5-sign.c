e70cecdbc268b058e17ff1a83d8127e491e495bd
Dephuture <tenkorangbright@gmail.com>
2023-07-18 17:58:37 -0700

    kk

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - this task is part of the function projects
+ * @n: letter
+ * Return: Always return 0 for success
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+	_putchar('\n');
+}
