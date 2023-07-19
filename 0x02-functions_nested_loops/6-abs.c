f4ae5ab3a4e8d6915303a7a4778305e8e1309812
Dephuture <tenkorangbright@gmail.com>
2023-07-18 18:43:13 -0700

    hh

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - this task is part of the function projects
+ * @i: letter
+ * Return: Always return 0 for success
+ */
+
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
