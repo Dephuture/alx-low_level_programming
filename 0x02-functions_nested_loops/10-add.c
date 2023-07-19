1f6e17162ea0e07829a2f1f59379bd7df5a993d3
Dephuture <tenkorangbright@gmail.com>
2023-07-19 10:26:13 -0700

    rr

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - this task is part of the function projects
+ * @a: the first integer
+ * @b: the second interger
+ * Return: Always return 0 for success
+ */
+
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
