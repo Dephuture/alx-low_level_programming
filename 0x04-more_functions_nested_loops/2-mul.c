b97ffadef87ba7953070a7ce0f576a60cf1d895a
Dephuture <tenkorangbright@gmail.com>
2023-07-21 12:42:08 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - checks whether is upper
+ * @a: checks the character
+ * @b: checks the character
+ * Return: returns 0 when upper  and 1 when not
+ */
+
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
