8fa894961483f7b0198726f450fbb0536b020cb7
Dephuture <tenkorangbright@gmail.com>
2023-07-21 12:35:29 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isdigit - checks whether is upper
+ * @c: checks the character
+ * Return: returns 0 when upper  and 1 when not
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+	return (1);
+	}
+	return (0);
+}
