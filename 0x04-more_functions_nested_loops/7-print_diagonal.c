ee098dc83e95a3e7f9c517de7a59918c331ffb71
Dephuture <tenkorangbright@gmail.com>
2023-07-21 18:01:51 -0700

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_diagonal - checks whether is upper
+ * @n: number of lines
+ * Return: returns 0 when upper  and 1 when not
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
