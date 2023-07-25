ea142d85175aaeb130b5dd3047e7083deb6ee6cf
Dephuture <tenkorangbright@gmail.com>
2023-07-24 19:59:35 -0700

    rev string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - accepts pointers
+ * @s: take a point
+ * Return: returns 0 if successf
+ */
+
+void rev_string(char *s)
+{
+	int length = 0;
+	int i;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	for (i = 0; i < length / 2; i++)
+	{
+		char temp = s[i];
+
+		s[i] = s[length - 1 - i];
+		s[length - 1 - i] = temp;
+	}
+}
