535b0e18f49a2ec2dbda1699af3f047cc8f5bc81
Dephuture <tenkorangbright@gmail.com>
2023-07-27 13:06:24 -0700

    101-print_number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_number - Prints an integer using _putchar function.
+ * @n: The integer to be printed.
+ */
+
+void print_number(int n)
+{
+	int divisor = 1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	while (n / divisor >= 10)
+	{
+		divisor *= 10;
+	}
+	while (divisor > 0)
+	{
+		int digit = n / divisor;
+
+		_putchar('0' + digit);
+		n %= divisor;
+		divisor /= 10;
+	}
+}
