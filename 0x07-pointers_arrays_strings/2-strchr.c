6b3fdb547518f5794226574c765b39f5eb913055
Dephuture <tenkorangbright@gmail.com>
2023-07-31 11:32:13 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * *_strchr - works as memset
+ * @s: takes an int
+ * @c: takes an int
+ * Return: returns 0 when successful
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+	return NULL;
+}
