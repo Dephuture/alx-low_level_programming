cdc8f4dd0c3eb7d0fc700cda46484f4929d40352
Dephuture <tenkorangbright@gmail.com>
2023-07-31 17:26:07 -0700

    set string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * set_string - works as memset
+ * @s: takes an int
+ * @to: takes int
+ * Return: returns 0 when successful
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
